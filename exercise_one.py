570cf038ea3f595109a3e2bb9f06967b64c67a54
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-21 14:08:27 +0200

    continuty on exercises

--- exercise_one.py
+++ exercise_one.py
@@ -5,3 +5,12 @@ birth_year = input("What year were you born \n")
 age = 2020 - int(birth_year)
 
 print(f"Your age is {age}")
+
+# password checker
+
+username = input("Enter your username \n")
+password_one = input("Enter your password \n")
+
+number = len(password_one)
+hashed = "*"*number
+print(f"Hey {username} your password {hashed} is {number} letters long")
