4dedec153ad9765ed04c002cec07af2456be53bd
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-21 13:11:03 +0200

    formated strings

---
+++ formatedstring.py
@@ -0,0 +1,11 @@
+# formated string
+name = "Aime"
+age = 25
+print("Hi "+name+" you are "+str(age)+" years old")
+
+# print in formated string better way
+
+print(f"Hi {name} you are {age} years old")
+
+# print in formates string python 2
+print("Hi {0} you are {1} years old".format(name,age))
