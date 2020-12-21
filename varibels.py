1094c7bc38c2578e1a9b0555cb0ec38cb725ee92
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-21 12:48:22 +0200

    update on variables

--- varibels.py
+++ varibels.py
@@ -20,3 +20,23 @@ __file__
 # asign value for may variables at once
 a,b,c = 1,2,3
 print(b)
+
+
+# strings 
+
+print(type("Hi am Aime Malaika"))
+
+user_name = "supercode"
+password = "supers"
+
+# notation for long strings
+long_strings = '''
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+'''
+
+print(long_strings)
+
+# concatenation for string 
+
+print(user_name+" "+password)
+
