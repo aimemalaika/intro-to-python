8afdf18baa1a1c07d8151384387012cbb99a419d
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-21 12:33:04 +0200

    pythons variables

---
+++ varibels.py
@@ -0,0 +1,22 @@
+user_iq = 190
+
+print(user_iq)
+
+# start with userscore for private variables
+_user_name = "Malaika"
+
+print(_user_name)
+
+
+# conatant are in capital letters
+
+PI = 3.14
+
+print(PI)
+
+# dunder variables
+__file__ 
+
+# asign value for may variables at once
+a,b,c = 1,2,3
+print(b)
