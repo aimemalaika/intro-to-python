41910663243130bd6e8c8301d73dba5e7386272a
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-21 13:21:41 +0200

    sings indexation

--- formatedstring.py
+++ formatedstring.py
@@ -9,3 +9,12 @@ print(f"Hi {name} you are {age} years old")
 
 # print in formates string python 2
 print("Hi {0} you are {1} years old".format(name,age))
+
+# sings indexation
+
+selfish = "01234567"
+
+#rule [start:stop:stepover]
+# use negative index to start at the end on the string
+print(selfish[0:8:3])
+print(selfish[::-2])
