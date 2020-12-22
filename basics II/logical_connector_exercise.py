5ef2894f511961c9fd6199b2ac1cdb7575c6d6c1
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-22 18:02:25 +0200

    done exercise

--- basics II/logical_connector_exercise.py
+++ basics II/logical_connector_exercise.py
@@ -10,3 +10,23 @@ elif is_magician and not(is_expert):
 
 elif not(is_expert):
   print("You are an expert")
+
+# find highest
+
+def highest_event(*args):
+  i = 0
+  temp = []
+  while i < len(args[0]):
+    if args[0][i] % 2 == 0:
+      temp.append(args[0][i])
+    i+=1
+  def get_max(temp):
+    highest = 0
+    for number in temp:
+      if number > highest:
+        highest = number
+    return highest
+
+  return get_max(temp)
+
+print(highest_event([10,2,1,4,6,3,8,11,66]))
