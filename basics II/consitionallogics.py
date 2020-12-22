9c36d9d41ae2034bd67ca144db7aaddefdcc0a43
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-22 11:31:14 +0200

    consitional

--- basics II/consitionallogics.py
+++ basics II/consitionallogics.py
@@ -5,7 +5,7 @@ is_licenced = True
 
 if is_old:
   print("You can drive")
-elif :
+elif is_licenced:
   print("You have a licence")
 else:
   print("Go in prison")
@@ -14,3 +14,9 @@ if is_old and is_licenced:
   print("You can drive")
 else:
   print("Go in prison")
+
+# tenary operators
+
+can_drive = "allowed" if is_old else "live the cart"
+
+print(can_drive)
