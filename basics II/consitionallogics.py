63805c99a6749fb022f07826d43c5ccebec14178
Aime Malaika <aimemalaika1995@gmail.com>
2020-12-22 11:15:24 +0200

    consitional

---
+++ basics II/consitionallogics.py
@@ -0,0 +1,16 @@
+# consitional logics
+
+is_old = True
+is_licenced = True
+
+if is_old:
+  print("You can drive")
+elif :
+  print("You have a licence")
+else:
+  print("Go in prison")
+
+if is_old and is_licenced:
+  print("You can drive")
+else:
+  print("Go in prison")
